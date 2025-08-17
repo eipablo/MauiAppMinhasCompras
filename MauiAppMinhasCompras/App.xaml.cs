3a4e4956118af194f04fda61d4167e231889cb99
Pablo Vinícius <71195004+eipablo@users.noreply.github.com>
2025-08-17 18:34:45 -0300

    editando pagina inicial

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -5,11 +5,6 @@
         public App()
         {
             InitializeComponent();
-        }
-
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            //return new Window(new AppShell());
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
